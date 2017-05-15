4552171721b9398255843fe9e1b15be0f00b2cda
Joshua Miller <jlmiller07@gmail.com>
2017-05-14 21:33:31 -0500

    adding inital html and js for keylogging and display of guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,29 @@
 
 
 
+// Game Object
+
+	var game = {
+		guessArray: [],
+		guessDisplay: "",
+		gameWordsArray: ["dog", "cat"],
+	}
+
+
+
+// User keystroke is logged and added to an array and then parsed to display without commas
+
+	document.onkeyup = function(e) {
+      	game.guessArray.push(e.key);
+      	game.guessDisplay = game.guessArray.join(" ");
+
+	// Keystroke guesses are injected back into HTML
+	    var htmlGuess = "<p>You chose: " + game.guessDisplay + "</p>";
+	    
+	    document.querySelector("#letters-guessed").innerHTML = htmlGuess;
+}
 
 
 
 
-// Here we create the HTML that will be injected into our div and displayed on the page.
-          var html = "<p>You chose: " + userChoice + "</p>Computer chose: " + computerChoice + "<p>wins: " + winScore + "</p>" +
-          "<p>losses: " + lossScore + "</p>" +
-          "<p>ties: " + tieScore + "</p>";
 
-          // Injecting the HTML we just created into our div and updating the game information on our page.
-          document.querySelector("#game").innerHTML = html; 
